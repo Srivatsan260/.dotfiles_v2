fb4f946646e8c0e74872ca5de88c5613a0e48cbc
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-10-16 12:35:29 +0530

    nvim-cmp - update cmp.mapping.complete

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -62,7 +62,9 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-/>'] = cmp.mapping.complete(),
+    ['<C-/>'] = cmp.mapping(cmp.mapping.complete({
+        reason = cmp.ContextReason.Auto,
+    }), {"i", "c"}),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
