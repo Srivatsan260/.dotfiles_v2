c234ab168597093eebb59e1e5c67d87a93b90d97
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-10-16 14:17:36 +0530

    nvim - add cmp-path and cmp-buffer

--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -27,6 +27,8 @@ return require('packer').startup(function()
     -- lsp
     use 'neovim/nvim-lspconfig'
     use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    use 'hrsh7th/cmp-path' -- nvim-cmp source for filesystem paths
+    use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
     use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
     use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
     use 'L3MON4D3/LuaSnip' -- Snippets plugin
