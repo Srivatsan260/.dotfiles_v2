47972f9faed02dc0ba096eed78747f941b555e37
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-10-25 12:23:37 +0530

    nvim-surround

--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -127,4 +127,15 @@ return require('packer').startup(function()
     -- interactive code evaluation
     use 'Olical/conjure'
     use 'PaterJason/cmp-conjure'
+
+    -- nvim-surround
+    use {
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    }
 end)
