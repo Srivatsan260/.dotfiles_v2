64ab274259d5bcc751fb2b35280fbb6c2629fe2d
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-11-06 18:48:13 +0530

    nvim - lsp refactor & move luasnip+nvim-cmp to completion.lua

---
+++ nvim/lua/plugins/completion.lua
@@ -0,0 +1,55 @@
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+luasnip.config.set_config {
+    history = true,
+    updateevents = "TextChanged,TextChangedI",
+    enable_autosnippets = true,
+}
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-/>'] = cmp.mapping(cmp.mapping.complete({
+            reason = cmp.ContextReason.Auto,
+        }), { "i", "c" }),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+        ['<C-k>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<C-j>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    }),
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
+        { name = 'vim-dadbod-completion' },
+        { name = 'conjure' },
+    },
+}
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,16 +1,3 @@
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-vim.keymap.set('n', '<leader>sc', '<cmd>lua print(vim.inspect(vim.lsp.buf_get_clients()[1].server_capabilities))<CR>',
-    opts)
-vim.keymap.set('n', '<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-end, opts)
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
 local on_attach = function(_, bufnr)
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -29,7 +16,7 @@ local on_attach = function(_, bufnr)
     vim.keymap.set('n', 'gW', vim.lsp.buf.workspace_symbol, bufopts)
     vim.keymap.set('n', '<leader>fa', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', '<leader>ar', vim.lsp.buf.rename, bufopts)
-    if vim.cmd [[echo &filetype ]] == 'python' then
+    if vim.bo.filetype == 'python' then
         vim.keymap.set('n', '<leader>-', '<cmd>!isort %<CR>', bufopts)
         vim.keymap.set('n', '<leader>=', '<cmd>!black %<CR>', bufopts)
     else
@@ -39,101 +26,60 @@ local on_attach = function(_, bufnr)
     vim.keymap.set('n', '<leader>ao', vim.lsp.buf.outgoing_calls, bufopts)
     vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
     vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, bufopts)
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, bufopts)
+    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, bufopts)
+    vim.keymap.set('n', '<leader>sc', '<cmd>lua print(vim.inspect(vim.lsp.buf_get_clients()[1].server_capabilities))<CR>',
+    bufopts)
+    vim.keymap.set('n', '<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
 end
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-require('neodev').setup {}
+require('neodev').setup {} -- neovim code helper
 
 local lspconfig = require('lspconfig')
-
--- luasnip setup
-local luasnip = require 'luasnip'
-
-luasnip.config.set_config {
-    history = true,
-    updateevents = "TextChanged,TextChangedI",
-    enable_autosnippets = true,
-}
-
--- nvim-cmp setup
-local cmp = require 'cmp'
-cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-/>'] = cmp.mapping(cmp.mapping.complete({
-            reason = cmp.ContextReason.Auto,
-        }), { "i", "c" }),
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-        ['<C-k>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<C-j>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-    }),
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'buffer' },
-        { name = 'path' },
-        { name = 'vim-dadbod-completion' },
-        { name = 'conjure' },
-    },
-}
 
 local lsp_flags = {
     -- This is the default in Nvim 0.7+
     debounce_text_changes = 150,
 }
 
-lspconfig['pyright'].setup {
-    on_attach = on_attach,
-    flags = lsp_flags,
-    capabilities = capabilities
-}
+local servers = { 'pyright', 'tsserver', 'html', 'cssls', 'emmet_ls' }
 
-lspconfig['tsserver'].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-lspconfig['html'].setup {
+for _, server in pairs(servers) do
+    lspconfig[server].setup {
+        on_attach = on_attach,
+        flags = lsp_flags,
+        capabilities = capabilities
+    }
+end
+
+-- lua lsp setup
+lspconfig.sumneko_lua.setup {
     on_attach = on_attach,
-    capabilities = capabilities,
-}
-lspconfig['cssls'].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
-lspconfig['emmet_ls'].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
+    settings = {
+        Lua = {
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+                enable = false,
+            },
+            completion = {
+                callSnippet = 'Replace'
+            }
+        }
+    }
 }
 
+-- rust lsp setup
 local rt = require('rust-tools')
 
 rt.setup {
@@ -150,33 +96,4 @@ rt.setup {
             vim.keymap.set('n', '<leader>=', vim.lsp.buf.format, bufopts)
         end,
     },
-}
-
-local runtime_path = vim.split(package.path, ";", {})
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
--- lua lsp setup
-lspconfig.sumneko_lua.setup {
-    on_attach = on_attach,
-    settings = {
-        Lua = {
-            runtime = {
-                version = 'LuaJIT',
-                path = runtime_path,
-            },
-            diagnostics = {
-                globals = { 'vim', 'use' },
-            },
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            telemetry = {
-                enable = false,
-            },
-            completion = {
-                callSnippet = 'Replace'
-            }
-        }
-    }
 }
