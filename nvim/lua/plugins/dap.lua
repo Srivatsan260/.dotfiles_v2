c6d576591590d8dbd80a2d0a61a642ec3fca0891
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-11-07 09:12:31 +0530

    nvim - dap.lua reformat code

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,9 +1,9 @@
 local dap = require('dap')
 
 dap.adapters.python = {
-  type = 'executable';
-  command = os.getenv("VIRTUAL_ENV") .. '/bin/python';
-  args = { '-m', 'debugpy.adapter' };
+    type = 'executable';
+    command = os.getenv("VIRTUAL_ENV") .. '/bin/python';
+    args = { '-m', 'debugpy.adapter' };
 }
 dap.configurations.python = {
     {
@@ -37,58 +37,58 @@ dap.configurations.rust = {
 
 -- require('dapui').setup {}
 require("dapui").setup({
-  icons = { expanded = "▾", collapsed = "▸" },
-  mappings = {
-    -- Use a table to apply multiple mappings
-    expand = { "<CR>", "<2-LeftMouse>" },
-    open = "o",
-    remove = "d",
-    edit = "e",
-    repl = "r",
-    toggle = "t",
-  },
-  -- Expand lines larger than the window
-  -- Requires >= 0.7
-  expand_lines = true,
-  -- Layouts define sections of the screen to place windows.
-  -- The position can be "left", "right", "top" or "bottom".
-  -- The size specifies the height/width depending on position. It can be an Int
-  -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-  -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-  -- Elements are the elements shown in the layout (in order).
-  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
-  layouts = {
-    {
-      elements = {
-      -- Elements can be strings or table with id and size keys.
-        { id = "scopes", size = 0.25 },
-        "breakpoints",
-        "stacks",
-        "watches",
-      },
-      size = 40, -- 40 columns
-      position = "left",
-    },
-    {
-      elements = {
-        "repl",
-        "console",
-      },
-      size = 0.25, -- 25% of total lines
-      position = "bottom",
-    },
-  },
-  floating = {
-    max_height = 0.3, -- These can be integers or a float between 0 and 1.
-    max_width = 0.3, -- Floats will be treated as percentage of your screen.
-    border = "single", -- Border style. Can be "single", "double" or "rounded"
+    icons = { expanded = "▾", collapsed = "▸" },
     mappings = {
-      close = { "q", "<Esc>" },
+        -- Use a table to apply multiple mappings
+        expand = { "<CR>", "<2-LeftMouse>" },
+        open = "o",
+        remove = "d",
+        edit = "e",
+        repl = "r",
+        toggle = "t",
     },
-  },
-  windows = { indent = 1 },
-  render = {
-    max_type_length = nil, -- Can be integer or nil.
-    max_value_lines = 100, -- Can be integer or nil.
-  }
+    -- Expand lines larger than the window
+    -- Requires >= 0.7
+    expand_lines = true,
+    -- Layouts define sections of the screen to place windows.
+    -- The position can be "left", "right", "top" or "bottom".
+    -- The size specifies the height/width depending on position. It can be an Int
+    -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+    -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+    -- Elements are the elements shown in the layout (in order).
+    -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+    layouts = {
+        {
+            elements = {
+                -- Elements can be strings or table with id and size keys.
+                { id = "scopes", size = 0.25 },
+                "breakpoints",
+                "stacks",
+                "watches",
+            },
+            size = 40, -- 40 columns
+            position = "left",
+        },
+        {
+            elements = {
+                "repl",
+                "console",
+            },
+            size = 0.25, -- 25% of total lines
+            position = "bottom",
+        },
+    },
+    floating = {
+        max_height = 0.3, -- These can be integers or a float between 0 and 1.
+        max_width = 0.3, -- Floats will be treated as percentage of your screen.
+        border = "single", -- Border style. Can be "single", "double" or "rounded"
+        mappings = {
+            close = { "q", "<Esc>" },
+        },
+    },
+    windows = { indent = 1 },
+    render = {
+        max_type_length = nil, -- Can be integer or nil.
+        max_value_lines = 100, -- Can be integer or nil.
+    }
 })
