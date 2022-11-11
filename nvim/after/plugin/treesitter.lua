b4135704029f03b942ab0466b9e3777810290607
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-11-11 18:26:51 +0530

    nvim - disable smart_rename in treesitter because lsp.buf.rename() is way better

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -9,7 +9,7 @@ require'nvim-treesitter.configs'.setup {
     refactor = {
         highlight_definitions = {enable = true, clear_on_cursor_move = true},
         -- highlight_current_scope = { enable = true },
-        smart_rename = {enable = true, keymaps = {smart_rename = 'grr'}},
+        smart_rename = {enable = false, keymaps = {smart_rename = 'grr'}},
         navigation = {
             enable = true,
             keymaps = {
