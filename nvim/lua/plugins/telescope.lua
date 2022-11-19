3ba30b98ced032af7280b021b836cb50ff2e6c13
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-11-19 08:33:34 +0530

    nvim - telescope - add action to delete buffers

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,9 +1,30 @@
-require'telescope'.setup {
-    extensions = {session_picker = {sessions_dir = vim.g['sessions_dir']}}
+local actions = require('telescope.actions')
+local telescope = require('telescope')
+
+telescope.setup {
+    extensions = {session_picker = {sessions_dir = vim.g['sessions_dir']}},
+    pickers = {
+        buffers = {
+            show_all_buffers = true,
+            sort_lastused = true,
+            theme = "dropdown",
+            mappings = {
+                i = {
+                    ["<c-d>"] = actions.delete_buffer,
+                    ["<c-f>"] = actions.preview_scrolling_down,
+                    ["<c-b>"] = actions.preview_scrolling_up,
+                },
+                n = {
+                    ["<c-d>"] = actions.delete_buffer,
+                    ["<c-f>"] = actions.preview_scrolling_down,
+                    ["<c-b>"] = actions.preview_scrolling_up,
+                }
+            }
+        }
+    }
 }
-require('telescope').load_extension('sessions_picker')
-require('telescope').load_extension('harpoon')
+telescope.load_extension('sessions_picker')
+telescope.load_extension('harpoon')
+telescope.load_extension('git_worktree')
 
 require('dir-telescope').setup {respect_gitignore = true}
-
-require('telescope').load_extension('git_worktree')
