148907293ce6cdb7780ee15bb7636a11c8737cdf
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-11-28 20:47:02 +0530

    wezterm - text bg opacity

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require('wezterm')
 return {
     hide_tab_bar_if_only_one_tab = true,
     window_background_opacity = 0.87,
+    text_background_opacity = 0.87,
     window_decorations = "RESIZE",
     color_scheme = 'Tomorrow Night Bright',
     font = wezterm.font('JetbrainsMono Nerd Font Mono'),
