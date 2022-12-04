b3504b781ce151bde4c6cdba0e4ad44ece7d9a1f
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-12-04 22:52:33 +0530

    nvim - bootstrap packer.nvim

--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -1,7 +1,27 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({
+            'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+            install_path
+        })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+-- vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
+
+    if packer_bootstrap then
+        require("packer").sync()
+    end
+
     use 'nvim-lua/plenary.nvim'
 
     -- themes
