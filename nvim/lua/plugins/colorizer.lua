122735bd8f97b89ba7cc99dfca84187d90d292dd
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-12-04 12:13:35 +0530

    nvim - colorizer - enable highlighting for all filetypes

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,5 +1,6 @@
 require("colorizer").setup({
     css = {hsl_fn = true, rgb_fn = true, names = true},
     'javascript',
-    html = {mode = 'foreground'}
+    html = {mode = 'foreground'},
+    '*',
 })
