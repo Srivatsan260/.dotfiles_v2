d92b377b271c1295ad4082857454eb115fd6676e
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-12-04 12:11:02 +0530

    nvim - lsp virtual text highlights

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -4,3 +4,9 @@ vim.cmd(string.format("colorscheme %s", vim.g['global_colorscheme']))
 
 vim.api.nvim_set_hl(0, "Folded", {bg = 'none', fg = '#78a9ff'})
 vim.api.nvim_set_hl(0, "CursorLineNr", {bg = 'none', fg = '#78a9ff'})
+
+-- TODO: figure out how to do this using nvim_get_hl_by_name
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", {bg = 'none', fg = '#db4b4b'})
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", {bg = 'none', fg = '#e0af68'})
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", {bg = 'none', fg = '#0db9d7'})
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", {bg = 'none', fg = '#1abc9c'})
