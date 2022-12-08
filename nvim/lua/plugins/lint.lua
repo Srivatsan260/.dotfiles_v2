144462cd2238b9ff1f53d368e4b4425c0b28a928
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-12-08 21:44:39 +0530

    nvim - shellcheck lint

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -1,8 +1,14 @@
-require('lint').linters_by_ft = {python = {'mypy', 'flake8'}}
+require('lint').linters_by_ft = {python = {'mypy', 'flake8'}, sh = {'shellcheck'}}
 
 local group = vim.api.nvim_create_augroup("LintGroup", {clear = true})
 vim.api.nvim_create_autocmd({"BufEnter", "BufWritePost"}, {
     group = group,
     pattern = "*.py",
+    callback = function() require("lint").try_lint() end
+})
+
+vim.api.nvim_create_autocmd({"BufEnter", "BufWritePost"}, {
+    group = group,
+    pattern = "*.sh",
     callback = function() require("lint").try_lint() end
 })
