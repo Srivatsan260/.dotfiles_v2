8a17b736c7c31c337aa53e0885207aa28615d7b0
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-12-12 12:07:58 +0530

    nvim - add sqlfluff lint

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -1,12 +1,13 @@
 require('lint').linters_by_ft = {
     python = {'mypy', 'flake8'},
     sh = {'shellcheck'},
-    lua = {'luacheck'}
+    lua = {'luacheck'},
+    sql = {'sqlfluff'},
 }
 
 local group = vim.api.nvim_create_augroup("LintGroup", {clear = true})
 vim.api.nvim_create_autocmd({"BufEnter", "BufWritePost"}, {
     group = group,
-    pattern = { "*.py", "*.sh", "*.lua" },
+    pattern = { "*.py", "*.sh", "*.lua", "*.sql"},
     callback = function() require("lint").try_lint() end
 })
