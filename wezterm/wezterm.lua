34c6958619a9f18ad596a665a93787285b020682
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-12-18 13:47:14 +0530

    wezterm - background updates

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require('wezterm')
 local home = os.getenv("HOME")
+local wallpaper_path = string.gsub("~/Documents/anime/mythra.png", "~", "")
 
 return {
     audible_bell = 'Disabled',
@@ -8,8 +9,8 @@ return {
     hide_tab_bar_if_only_one_tab = true,
     background = {
         {
-            source = {File = home .. '/Documents/anime/green-eyes.jpg'},
-            repeat_x = 'NoRepeat',
+            source = {File = home .. wallpaper_path},
+            repeat_x = "NoRepeat",
             hsb = {brightness = 0.03}
         }
     },
@@ -18,9 +19,6 @@ return {
     window_decorations = "RESIZE",
 
     color_scheme = 'Tomorrow Night Bright',
-    font = wezterm.font {
-        family = 'JetbrainsMono Nerd Font Mono',
-        weight = 'Medium'
-    },
+    font = wezterm.font {family = 'JetbrainsMono Nerd Font Mono', weight = 'Medium'},
     font_size = 15
 }
