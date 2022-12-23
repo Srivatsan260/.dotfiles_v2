51cc1bd47bd6fe218642a8abc8161067d9b2d852
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-12-23 13:54:01 +0530

    nvim - lint json

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -8,7 +8,7 @@ require('lint').linters_by_ft = {
 local group = vim.api.nvim_create_augroup("LintGroup", {clear = true})
 vim.api.nvim_create_autocmd({"BufEnter", "BufWritePost"}, {
     group = group,
-    pattern = {"*.py", "*.sh"},
+    pattern = {"*.py", "*.sh", "*.json"},
     callback = function()
         require("lint").try_lint()
     end
