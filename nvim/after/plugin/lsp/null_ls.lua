c7650dcc37703a1fa98ebe8dc5438763ca11fd76
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-12-26 20:45:13 +0530

    null_ls - sqlfluff formatting

--- nvim/after/plugin/lsp/null_ls.lua
+++ nvim/after/plugin/lsp/null_ls.lua
@@ -12,6 +12,9 @@ null_ls.setup({
         null_ls.builtins.diagnostics.sqlfluff.with({
             extra_args = { "--dialect", "snowflake" },
         }),
+        null_ls.builtins.formatting.sqlfluff.with({
+            extra_args = { "--dialect", "snowflake" },
+        }),
         null_ls.builtins.diagnostics.todo_comments,
     },
 })
