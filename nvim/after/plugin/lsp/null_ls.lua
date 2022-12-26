56dd0d1724d3b8b753645f1288c3f9e7e6c160fc
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-12-26 17:12:36 +0530

    null_ls - add black, isort

--- nvim/after/plugin/lsp/null_ls.lua
+++ nvim/after/plugin/lsp/null_ls.lua
@@ -4,6 +4,8 @@ null_ls.setup({
     sources = {
         null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.code_actions.shellcheck,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
         null_ls.builtins.diagnostics.mypy,
         null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.diagnostics.rstcheck,
