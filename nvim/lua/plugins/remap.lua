31a84476e20ec931f05ade24dda0c50250a9768e
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-12-30 10:25:28 +0530

    reformat

--- nvim/lua/plugins/remap.lua
+++ nvim/lua/plugins/remap.lua
@@ -76,24 +76,32 @@ vim.keymap.set("t", "<C-l>", function()
 end, {expr = true, desc = "window right (terminal mode)"})
 
 -- treesitter highlighting
-vim.keymap.set("n", "<leader>th", "<cmd>TSBufToggle highlight<CR>", {desc = "toggle treesitter highlights"})
-vim.keymap.set("n", "<leader>tp", "<cmd>TSPlaygroundToggle<CR>", {desc = "toggle treesitter playground"})
+vim.keymap.set("n", "<leader>th", "<cmd>TSBufToggle highlight<CR>",
+    {desc = "toggle treesitter highlights"})
+vim.keymap.set("n", "<leader>tp", "<cmd>TSPlaygroundToggle<CR>",
+    {desc = "toggle treesitter playground"})
 
 vim.keymap.set("n", "<leader>tj", "<cmd>TSJToggle<CR>", {desc = "toggle treesj join"})
 
 -- FZF / telescope / lazygit
-vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files hidden=True theme=dropdown<CR>", {desc = "Telescope find_files"})
-vim.keymap.set("n", "<leader>fb", "<cmd>Telescope file_browser hidden=True theme=dropdown<CR>", {desc = "Telescope file_browser"})
+vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files hidden=True theme=dropdown<CR>",
+    {desc = "Telescope find_files"})
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope file_browser hidden=True theme=dropdown<CR>",
+    {desc = "Telescope file_browser"})
 -- vim.keymap.set("n", "<C-p>", "<cmd>Files<CR>")
-vim.keymap.set("n", "<leader>cs", "<cmd>Telescope colorscheme<CR>", {desc = "List colorschemes in telescope"})
+vim.keymap.set("n", "<leader>cs", "<cmd>Telescope colorscheme<CR>",
+    {desc = "List colorschemes in telescope"})
 vim.keymap.set("n", "<leader>ct", "<cmd>! ctags -R<CR>", {desc = "generate ctags"})
 vim.keymap.set("n", "<leader>fd", "<cmd>FileInDirectory<CR>", {desc = "find file in directory"})
 vim.keymap.set("n", "<leader>pd", "<cmd>GrepInDirectory<CR>", {desc = "grep in directory"})
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope live_grep<CR>", {desc = "grep in workspace"})
-vim.keymap.set("n", "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find theme=ivy<CR>", {desc = "fuzzy find in current buffer"})
-vim.keymap.set("n", "<leader>?", "<cmd>Telescope oldfiles theme=ivy<CR>", {desc = "show recent files using telescope"})
+vim.keymap.set("n", "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find theme=ivy<CR>",
+    {desc = "fuzzy find in current buffer"})
+vim.keymap.set("n", "<leader>?", "<cmd>Telescope oldfiles theme=ivy<CR>",
+    {desc = "show recent files using telescope"})
 vim.keymap.set("n", "<leader>b", "<cmd>Telescope buffers<CR>", {desc = "list open buffers"})
-vim.keymap.set("n", "<leader>cd", "<cmd>Telescope zoxide list<CR>", {desc = "list directories using zoxide"})
+vim.keymap.set("n", "<leader>cd", "<cmd>Telescope zoxide list<CR>",
+    {desc = "list directories using zoxide"})
 
 vim.keymap.set("n", "<Tab>", function()
     return vim.bo.filetype ~= "floaterm" and "<cmd>bn<CR>" or "<Tab>"
@@ -102,36 +110,48 @@ vim.keymap.set("n", "<S-Tab>", function()
     return vim.bo.filetype ~= "floaterm" and "<cmd>bp<CR>" or "<Tab>"
 end, {expr = true, desc = "previous buffer"})
 
-vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", {desc = "show help tags in Telescope"})
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>",
+    {desc = "show help tags in Telescope"})
 -- INFO: This remap works only if LSP is running (obviously) :/
-vim.keymap.set("n", "<leader>tg", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>", {desc = "list all symbols in workspace (uses LSP)"})
-vim.keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches theme=ivy<CR>", {desc = "list git branches"})
+vim.keymap.set("n", "<leader>tg", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>",
+    {desc = "list all symbols in workspace (uses LSP)"})
+vim.keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches theme=ivy<CR>",
+    {desc = "list git branches"})
 vim.keymap.set({"n", "v"}, "]g", "<cmd>Gitsigns next_hunk<CR>", {desc = "next hunk in buffer"})
 vim.keymap.set({"n", "v"}, "[g", "<cmd>Gitsigns prev_hunk<CR>", {desc = "prev hunk in buffer"})
-vim.keymap.set("n", "<leader>P", "<cmd>Gitsigns preview_hunk_inline<CR>", {desc = "preview current hunk inline"})
+vim.keymap.set("n", "<leader>P", "<cmd>Gitsigns preview_hunk_inline<CR>",
+    {desc = "preview current hunk inline"})
 vim.keymap.set("n", "<leader>R", "<cmd>Gitsigns reset_hunk<CR>", {desc = "reset current hunk"})
 vim.keymap.set("n", "<leader>S", "<cmd>Gitsigns stage_hunk<CR>", {desc = "stage current hunk"})
-vim.keymap.set("v", "<leader>S", ":'<,'>Gitsigns stage_hunk<CR>", {desc = "stage current hunk (visual mode)"})
-vim.keymap.set("n", "<leader>U", "<cmd>Gitsigns undo_stage_hunk<CR>", {desc = "unstage current hunk"})
-vim.keymap.set("v", "<leader>U", ":'<,'>Gitsigns undo_stage_hunk<CR>", {desc = "unstage current hunk (visual mode)"})
+vim.keymap.set("v", "<leader>S", ":'<,'>Gitsigns stage_hunk<CR>",
+    {desc = "stage current hunk (visual mode)"})
+vim.keymap.set("n", "<leader>U", "<cmd>Gitsigns undo_stage_hunk<CR>",
+    {desc = "unstage current hunk"})
+vim.keymap.set("v", "<leader>U", ":'<,'>Gitsigns undo_stage_hunk<CR>",
+    {desc = "unstage current hunk (visual mode)"})
 vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>", {desc = "Open lazygit"})
 vim.keymap.set("n", "<leader>gO", "<cmd>LazyGitConfig<CR>", {desc = "Open lazygit config file"})
 vim.keymap.set("n", "<leader>gf", "<cmd>GitFiles<CR>", {desc = "list git files (FZF)"})
 vim.keymap.set("n", "<leader>gn", ":Git checkout -b ", {desc = "checkout new branch"})
-vim.keymap.set("n", "<leader>gr", "<cmd>Telescope grep_string<CR>", {desc = "grep for cword in workspace"})
+vim.keymap.set("n", "<leader>gr", "<cmd>Telescope grep_string<CR>",
+    {desc = "grep for cword in workspace"})
 vim.keymap.set("n", "<leader>gR", function()
     local search = vim.fn.input({prompt = "Search for: ", default = ""})
     if search == "" then return end
     vim.cmd("Telescope grep_string search=" .. search)
 end, {desc = "grep for user input in workspace"})
-vim.keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<CR>", {desc = "show git status in Telescope"})
+vim.keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<CR>",
+    {desc = "show git status in Telescope"})
 -- vim.keymap.set("n", "<leader>gl", "<cmd>Telescope git_bcommits<CR>")
-vim.keymap.set("n", "<leader>gl", "<cmd>LazyGitFilterCurrentFile<CR>", {desc = "show git log for current file in lazygit"})
+vim.keymap.set("n", "<leader>gl", "<cmd>LazyGitFilterCurrentFile<CR>",
+    {desc = "show git log for current file in lazygit"})
 vim.keymap.set("n", "<leader>dvh", "<cmd>DiffviewOpen HEAD~1<CR>", {desc = "dif HEAD with HEAD~1"})
 vim.keymap.set("n", "<leader>dvH", ":DiffviewOpen HEAD~", {desc = "diff HEAD with HEAD~n"})
-vim.keymap.set("n", "<leader>dvl", "<cmd>DiffviewFileHistory<CR>", {desc = "git log in diff window"})
+vim.keymap
+    .set("n", "<leader>dvl", "<cmd>DiffviewFileHistory<CR>", {desc = "git log in diff window"})
 vim.keymap.set("n", "<leader>dvo", "<cmd>DiffviewOpen<CR>", {desc = "open Diffview"})
-vim.keymap.set("n", "<leader>ft", "<cmd>Telescope filetypes<CR>", {desc = "list available filetypes in Telescope"})
+vim.keymap.set("n", "<leader>ft", "<cmd>Telescope filetypes<CR>",
+    {desc = "list available filetypes in Telescope"})
 
 vim.keymap.set("n", "<leader>gwl", function()
     require("telescope").extensions.git_worktree.git_worktrees()
@@ -198,13 +218,15 @@ vim.keymap.set("n", "<leader>gP", async_git_op("push"), {desc = "async git push"
 vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<CR>", {desc = "toggle undotree"})
 
 -- search & replace current word
-vim.keymap.set("n", "<leader>sr", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", {desc = "search and replace cword in buffer"})
+vim.keymap.set("n", "<leader>sr", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
+    {desc = "search and replace cword in buffer"})
 
 -- dap
 vim.keymap.set("n", "<leader>du", function()
     require('dapui').toggle()
 end, {desc = "toggle dap ui"})
-vim.keymap.set("n", "<leader>db", "<cmd>DapToggleBreakpoint<CR>", {desc = "toggle debug breakpoint"})
+vim.keymap
+    .set("n", "<leader>db", "<cmd>DapToggleBreakpoint<CR>", {desc = "toggle debug breakpoint"})
 vim.keymap.set("n", "<leader>dB", function()
     require('dap').set_breakpoint(vim.fn.input('breakpoint condition:'))
 end, {desc = "toggle conditional breakpoint"})
@@ -212,10 +234,12 @@ vim.keymap.set("n", "<leader>dc", "<cmd>DapContinue<CR>", {desc = "debug continu
 vim.keymap.set("n", "<leader>di", "<cmd>DapStepInto<CR>", {desc = "debug step into"})
 vim.keymap.set("n", "<leader>do", "<cmd>DapStepOut<CR>", {desc = "debug step out"})
 vim.keymap.set("n", "<leader>dO", "<cmd>DapStepOver<CR>", {desc = "debug step over"})
-vim.keymap.set("n", "<leader>dt", "<cmd>DapTerminate<CR>", {desc = "terminate current debug session"})
+vim.keymap.set("n", "<leader>dt", "<cmd>DapTerminate<CR>",
+    {desc = "terminate current debug session"})
 
 -- Trouble
-vim.keymap.set("n", "<leader>tr", "<cmd>TroubleToggle document_diagnostics<CR>", {desc = "show document_diagnostics in Trouble window"})
+vim.keymap.set("n", "<leader>tr", "<cmd>TroubleToggle document_diagnostics<CR>",
+    {desc = "show document_diagnostics in Trouble window"})
 
 -- select all with ctrl-a
 -- vim.keymap.set("n", "<C-a>", "ggVG")
@@ -246,14 +270,17 @@ vim.keymap.set("n", "<localleader>rp", "<Plug>RestNvimPreview<CR>")
 vim.keymap.set("n", "<localleader>rl", "<Plug>RestNvimLast<CR>")
 
 -- delete all buffers except current
-vim.keymap.set("n", "<leader>ca", ":wa <bar> %bd <bar> e# <bar> bd# <CR><CR>", {desc = "delete all buffers except current"})
+vim.keymap.set("n", "<leader>ca", ":wa <bar> %bd <bar> e# <bar> bd# <CR><CR>",
+    {desc = "delete all buffers except current"})
 
 -- add zz to nav remaps
 vim.keymap.set("n", "<C-d>", "<C-d>zz", {desc = "ctrl-d and center"})
 vim.keymap.set("n", "<C-u>", "<C-u>zz", {desc = "ctrl-u and center"})
 vim.keymap.set("n", "G", "Gzz", {desc = "G and center"})
-vim.keymap.set("n", "n", "nzzzv", {desc = "next search result and center while not opening all the folds"})
-vim.keymap.set("n", "N", "Nzzzv", {desc = "previous search result and center while not opening all the folds"})
+vim.keymap.set("n", "n", "nzzzv",
+    {desc = "next search result and center while not opening all the folds"})
+vim.keymap.set("n", "N", "Nzzzv",
+    {desc = "previous search result and center while not opening all the folds"})
 vim.keymap.set("n", "<C-o>", "<C-o>zz", {desc = "older position in jumplist and center"})
 vim.keymap.set("n", "<C-i>", "<C-i>zz", {desc = "newer position in jumplist and center"})
 vim.keymap.set("c", "<CR>", function()
@@ -295,7 +322,8 @@ vim.keymap.set("n", "<leader>x", "<cmd>RunFile<CR>", {desc = "run current file u
 
 -- clearjumps
 vim.keymap.set("n", "<leader>cj", "<cmd>clearjumps<CR>", {desc = "clear jumplist"})
-vim.keymap.set("n", "<leader>jj", "<cmd>Telescope jumplist<CR>", {desc = "open jumplist in Telescope"})
+vim.keymap.set("n", "<leader>jj", "<cmd>Telescope jumplist<CR>",
+    {desc = "open jumplist in Telescope"})
 
 -- open zshrc and zshenv
 vim.keymap.set("n", "<leader>nz", "<cmd>e ~/.zshrc<CR>", {desc = "open .zshrc"})
@@ -305,8 +333,11 @@ vim.keymap.set("n", "<leader>ne", "<cmd>e ~/.zshenv<CR>", {desc = "open .zshenv"
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, {desc = "open diagnostic float"})
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {desc = "goto previous diagnostic"})
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {desc = "goto next diagnostic"})
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, {desc = "add buffer diagnostics to loclist"})
-if vim.bo.filetype ~= "lua" then vim.keymap.set('n', '<leader>=', vim.lsp.buf.format, {desc = "lsp buf format"}) end
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist,
+    {desc = "add buffer diagnostics to loclist"})
+if vim.bo.filetype ~= "lua" then
+    vim.keymap.set('n', '<leader>=', vim.lsp.buf.format, {desc = "lsp buf format"})
+end
 
 -- more textobjs
 vim.keymap.set({"o", "x"}, "iS", function()
