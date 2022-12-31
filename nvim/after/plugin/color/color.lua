1730056c013c4a7fa4cdfe5b73272a26fd16847e
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2022-12-31 08:48:40 +0530

    add TODO

--- nvim/after/plugin/color/color.lua
+++ nvim/after/plugin/color/color.lua
@@ -1,6 +1,7 @@
 pcall(require, "themes." .. vim.g["global_colorscheme"])
 vim.cmd(string.format("colorscheme %s", vim.g['global_colorscheme']))
 
+-- TODO set this according to theme?
 vim.api.nvim_set_hl(0, "Folded", {bg = 'none', fg = '#78a9ff'})
 vim.api.nvim_set_hl(0, "CursorLineNr", {bg = 'none', fg = '#78a9ff'})
 
