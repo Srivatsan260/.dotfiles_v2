dca81b5192a405caa27b8b4af7b869a29f812704
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-01-04 16:46:58 +0530

    simplify code

--- nvim/lua/themes/init.lua
+++ nvim/lua/themes/init.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.themes = {}
-local f = io.popen("ls " .. "nvim/lua/themes/")
+local f = io.popen("ls nvim/lua/themes/")
 if f then
     for name in f:lines() do
         if name ~= "init.lua" then
