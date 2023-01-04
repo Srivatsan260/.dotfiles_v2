9235f4b3b15f56b6a9820e2a04e9fccc11d3fb6f
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-01-04 16:45:59 +0530

    nvim - generate themes enum

--- nvim/lua/themes/init.lua
+++ nvim/lua/themes/init.lua
@@ -1,13 +1,14 @@
 local M = {}
 
--- TODO: automate this enum by listing themes dir and getting file names
-M.themes = {
-    catppuccin = "catppuccin",
-    gruvbox = "gruvbox",
-    kanagawa = "kanagawa",
-    onedark = "onedark",
-    rosepine = "rosepine",
-    tokyonight = "tokyonight",
-}
+M.themes = {}
+local f = io.popen("ls " .. "nvim/lua/themes/")
+if f then
+    for name in f:lines() do
+        if name ~= "init.lua" then
+            local value = string.gsub(name, ".lua", "")
+            M.themes[value] = value
+        end
+    end
+end
 
 return M
