e1d08860d254c5dd9826d535d42c38e0951b5572
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-01-05 08:23:15 +0530

    themes/init.lua - fix path

--- nvim/lua/themes/init.lua
+++ nvim/lua/themes/init.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.themes = {}
-local f = io.popen("ls nvim/lua/themes/")
+local f = io.popen("ls " .. vim.fn.stdpath("config") .. "/lua/themes/")
 if f then
     for name in f:lines() do
         if name ~= "init.lua" then
