c813d0e0d383817fc637b257126ad479ecc321ee
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-01-24 22:39:54 +0530

    does this even work?

--- nvim/after/plugin/treesitter/ts_node_action.lua
+++ nvim/after/plugin/treesitter/ts_node_action.lua
@@ -3,11 +3,4 @@ if not tna_ok then return end
 
 local helpers = require("ts-node-action.helpers")
 
-ts_node_action.setup({
-    lua = {
-        ["false"] = ts_node_action.toggle_boolean,
-        ["true"] = ts_node_action.toggle_boolean,
-        ["table_constructor"] = ts_node_action.toggle_multiline,
-        ["arguments"] = ts_node_action.toggle_multiline,
-    },
-})
+ts_node_action.setup({})
