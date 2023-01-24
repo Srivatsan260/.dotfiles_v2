8440c8a5e76dbe00841ce1e876530af4d7780f27
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-01-24 19:33:13 +0530

    various-textobjs - lookforward 5 lines

--- nvim/after/plugin/textobj/textobj.lua
+++ nvim/after/plugin/textobj/textobj.lua
@@ -2,6 +2,6 @@ local t_ok, textobjs = pcall(require, "various-textobjs")
 if not t_ok then return end
 
 textobjs.setup {
-    lookForwardLines = 0, -- default: 5. Set to 0 to only look in the current line
+    lookForwardLines = 5, -- default: 5. Set to 0 to only look in the current line
     useDefaultKeymaps = false -- Use suggested keymaps (see README). Default: false.
 }
