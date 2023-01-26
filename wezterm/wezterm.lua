7bf405b4a317da3ed072e7e0b3f75876044c2387
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-01-26 17:30:24 +0530

    wezterm - move background to function

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,26 @@
 local wezterm = require('wezterm')
-local home = os.getenv("HOME")
-local wallpaper_path = string.gsub("~/Documents/anime/mythra.png", "~", "")
+
+local function get_background()
+    local home = os.getenv("HOME")
+    local wallpaper_path = string.gsub("~/Documents/wallpapers/anime/mythra.png", "~", "")
+    local opts = {
+        {
+            source = {File = home .. wallpaper_path},
+            repeat_x = "NoRepeat",
+            hsb = {brightness = 0.03},
+            vertical_align = "Middle"
+        }
+    }
+    return {}
+    -- return opts
+end
 
 return {
     audible_bell = 'Disabled',
     max_fps = 120,
 
     hide_tab_bar_if_only_one_tab = true,
-    background = {
-        {
-            source = {File = home .. wallpaper_path},
-            repeat_x = "NoRepeat",
-            hsb = {brightness = 0.03}
-        }
-    },
+    background = get_background(),
     window_background_opacity = 0.85,
     text_background_opacity = 0.90,
     window_decorations = "RESIZE",
