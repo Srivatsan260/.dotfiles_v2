6217d0a80a5a5399de159a71d38e5fcfc0a80aa2
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-01-28 16:22:03 +0530

    nvim - use rust-tools instead of rust_analyzer via Mason

--- nvim/after/plugin/lsp/lsp_zero.lua
+++ nvim/after/plugin/lsp/lsp_zero.lua
@@ -7,9 +7,7 @@ if neodev_ok then neodev.setup {} end
 if not lsp_ok then return end
 
 lsp.preset('recommended')
-lsp.ensure_installed({
-    'pyright', 'tsserver', 'html', 'cssls', 'emmet_ls', 'rust_analyzer', 'sumneko_lua'
-})
+lsp.ensure_installed({'pyright', 'tsserver', 'html', 'cssls', 'emmet_ls', 'sumneko_lua'})
 
 lsp.set_preferences({
     set_lsp_keymaps = false,
@@ -79,3 +77,50 @@ vim.diagnostic.config({
     severity_sort = true,
     float = true
 })
+
+-- rust lsp setup
+local rt_ok, rt = pcall(require, 'rust-tools')
+if not rt_ok then return end
+
+rt.setup {
+    server = {
+        on_attach = function(_, bufnr)
+            vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+            local bufopts = function(desc)
+                return {noremap = true, silent = true, buffer = bufnr, desc = desc}
+            end
+            vim.keymap.set('n', 'K', rt.hover_actions.hover_actions, bufopts("hover actions"))
+            vim.keymap.set('n', '<leader>ac', rt.code_action_group.code_action_group, bufopts(""))
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts("goto declaration"))
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts("goto definition"))
+            vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, bufopts("goto implementation"))
+            vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<CR>', bufopts("find lsp references"))
+            vim.keymap.set('n', 'g,', vim.lsp.buf.signature_help, bufopts("signature help"))
+            vim.keymap.set('n', '<leader>=', vim.lsp.buf.format, bufopts("format document"))
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts("rename symbol"))
+            vim.keymap.set('n', '<leader>ai', vim.lsp.buf.incoming_calls,
+                bufopts("list incoming calls"))
+            vim.keymap.set('n', '<leader>ao', vim.lsp.buf.outgoing_calls,
+                bufopts("list outgoing calls"))
+            vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder,
+                bufopts("add workspace folder"))
+            vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder,
+                bufopts("remove workspace folder"))
+            vim.keymap.set('n', '<leader>sc', function()
+                print(vim.inspect(vim.lsp.get_active_clients()[1].server_capabilities))
+            end, bufopts("list lsp server capabilities"))
+            vim.keymap.set('n', '<leader>wl', function()
+                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            end, bufopts("list workspace folders"))
+            vim.keymap.set('n', '<leader>g,', vim.lsp.buf.signature_help, bufopts("signature help"))
+            vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation,
+                bufopts("goto implementation"))
+            vim.keymap.set('n', '<leader>gt', vim.lsp.buf.type_definition,
+                bufopts("goto type definition"))
+            vim.keymap.set('n', '<leader>ds', vim.lsp.buf.document_symbol,
+                bufopts("list document lsp symbols"))
+            vim.keymap.set('n', '<leader>dS', vim.lsp.buf.workspace_symbol,
+                bufopts("list workspace lsp symbols"))
+        end
+    }
+}
