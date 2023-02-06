e21514fd164da873ecf8eb0aa48f83a5ccf39cd3
Srivatsan260 <srivatsan260@gmail.com>
2023-02-06 17:57:58 +0530

    nvim - disable ensure_installed

--- nvim/after/plugin/treesitter/treesitter.lua
+++ nvim/after/plugin/treesitter/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = {"python", "rust", "sql", "yaml", "toml", "c", "http", "json"},
+    -- ensure_installed = {"python", "rust", "sql", "yaml", "toml", "c", "http", "json"},
     sync_install = false,
 
     highlight = {enable = true, additional_vim_regex_highlighting = false},
