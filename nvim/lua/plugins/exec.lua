66eca72067565cb5ffdbdceda120dae4fbbaeeaa
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-02-12 11:15:49 +0530

    nvim - lazy-load coderunner and vim-test

--- nvim/lua/plugins/exec.lua
+++ nvim/lua/plugins/exec.lua
@@ -1,4 +1,27 @@
 return {
-    'CRAG666/code_runner.nvim',
-    'vim-test/vim-test',
+    {
+        'CRAG666/code_runner.nvim',
+        opts = {
+            mode = 'term',
+            focus = true,
+            border = 'single',
+            filetype = {
+                python = "python -u",
+                rust = "cargo build && cargo run"
+            }
+        },
+        keys = {
+            {"<leader>x", "<cmd>RunFile<CR>", desc = "run current file using coderunner"},
+        },
+        lazy = true,
+    },
+    {
+        'vim-test/vim-test',
+        keys = {
+            {"<leader>tsn", "<cmd>TestNearest<CR>", desc = "test nearest"},
+            {"<leader>tsc", "<cmd>TestClass<CR>", desc = "test class"},
+            {"<leader>tsf", "<cmd>TestFile<CR>", desc = "test file"},
+        },
+        lazy = true,
+    },
 }
