4092e082446a6cf3f2d429b57acf353a9a2e86aa
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-02-12 19:38:10 +0530

    nvim - add ftplugin for lua to override formatoptions

---
+++ nvim/after/ftplugin/lua.lua
@@ -0,0 +1,9 @@
+vim.opt.formatoptions = vim.opt.formatoptions - "a" -- Auto formatting is BAD.
+- "t" -- Don't auto format my code. I got linters for that.
++ "c" -- When comments respect textwidth
++ "q" -- Allow formatting comments w/ gq
+- "o" -- O and o, don't continue comments
++ "r" -- But do continue when pressing enter.
++ "n" -- Indent past the formatlistpat, not underneath it.
++ "j" -- Auto-remove comments if possible.
+- "2" -- Don't use indent of 2nd line of paragraph for the rest of it
