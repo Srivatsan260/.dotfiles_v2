10d37edb783053e31fa29dcb8f00c8ce3872b555
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-02-12 15:03:52 +0530

    textobj.lua - fix remap

--- nvim/lua/plugins/textobj.lua
+++ nvim/lua/plugins/textobj.lua
@@ -6,13 +6,13 @@ return {
         useDefaultKeymaps = false -- Use suggested keymaps (see README). Default: false.
     },
     keys = {
-        {"iS", function() require("various-textobjs").subword(true) end, desc = "visual select in subword"},
-        {"aS", function() require("various-textobjs").subword(false) end, desc = "visual select around subword"},
-        {"iv", function() require("various-textobjs").value(true) end, desc = "visual select in value"},
-        {"av", function() require("various-textobjs").value(false) end, desc = "visual select around value"},
-        {"in", function() require("various-textobjs").number(true) end, desc = "visual select in number"},
-        {"an", function() require("various-textobjs").number(false) end, desc = "visual select around number"},
-        {"ii", function() require("various-textobjs").indentation(true, true) end, desc = "visual select in indent"},
-        {"ai", function() require("various-textobjs").indentation(false, false) end, desc = "visual select around indent"},
+        {"iS", function() require("various-textobjs").subword(true) end, desc = "visual select in subword", mode = {"o", "x"}},
+        {"aS", function() require("various-textobjs").subword(false) end, desc = "visual select around subword", mode = {"o", "x"}},
+        {"iv", function() require("various-textobjs").value(true) end, desc = "visual select in value", mode = {"o", "x"}},
+        {"av", function() require("various-textobjs").value(false) end, desc = "visual select around value", mode = {"o", "x"}},
+        {"in", function() require("various-textobjs").number(true) end, desc = "visual select in number", mode = {"o", "x"}},
+        {"an", function() require("various-textobjs").number(false) end, desc = "visual select around number", mode = {"o", "x"}},
+        {"ii", function() require("various-textobjs").indentation(true, true) end, desc = "visual select in indent", mode = {"o", "x"}},
+        {"ai", function() require("various-textobjs").indentation(false, false) end, desc = "visual select around indent", mode = {"o", "x"}},
     }
 }
