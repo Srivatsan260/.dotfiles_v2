dad5a688f5b3fb3436d33b1262f4175fb517bb5f
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-02-14 08:32:04 +0530

    indent.lua - update event, filetype_exclude

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -1,12 +1,21 @@
 return {
     {
         "lukas-reineke/indent-blankline.nvim",
-        event = {"BufReadPost", "BufNewFile"},
+        event = {"BufReadPre", "BufNewFile"},
         opts = {
             -- char = "▏",
             char = "│",
             filetype_exclude = {
-                "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason", "undotree"
+                "help",
+                "alpha",
+                "dashboard",
+                "neo-tree",
+                "Trouble",
+                "lazy",
+                "mason",
+                "undotree",
+                "qf",
+                "netrw",
             },
             show_trailing_blankline_indent = false,
             show_current_context = false
@@ -24,7 +33,16 @@ return {
         config = function(_, opts)
             vim.api.nvim_create_autocmd("FileType", {
                 pattern = {
-                    "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason", "undotree"
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "lazy",
+                    "mason",
+                    "undotree",
+                    "qf",
+                    "netrw",
                 },
                 callback = function()
                     vim.b.miniindentscope_disable = true
