b27dfbc60c5b488917089a51eb874f212c8bec50
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-02-15 13:08:35 +0530

    treesitter.lua - update config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,59 @@ return {
     {
         'nvim-treesitter/nvim-treesitter',
         dependencies = {
-            {'nvim-treesitter/nvim-treesitter-context', build = ":TSUpdate"},
+            {
+                'nvim-treesitter/nvim-treesitter-context',
+                build = ":TSUpdate",
+                config = function()
+                    require('treesitter-context').setup({
+                        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+                        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+                        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+                        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+                        patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+                            -- For all filetypes
+                            -- Note that setting an entry here replaces all other patterns for this entry.
+                            -- By setting the 'default' entry below, you can control which nodes you want to
+                            -- appear in the context window.
+                            default = {
+                                'class', 'function', 'method', 'for', 'while', 'if', 'switch',
+                                'case', 'interface', 'struct', 'enum'
+                            },
+                            -- Patterns for specific filetypes
+                            -- If a pattern is missing, *open a PR* so everyone can benefit.
+                            tex = {'chapter', 'section', 'subsection', 'subsubsection'},
+                            haskell = {'adt'},
+                            rust = {'impl_item'},
+                            terraform = {'block', 'object_elem', 'attribute'},
+                            scala = {'object_definition'},
+                            vhdl = {'process_statement', 'architecture_body', 'entity_declaration'},
+                            markdown = {'section'},
+                            elixir = {
+                                'anonymous_function', 'arguments', 'block', 'do_block', 'list',
+                                'map', 'tuple', 'quoted_content'
+                            },
+                            json = {'pair'},
+                            typescript = {'export_statement'},
+                            yaml = {'block_mapping_pair'}
+                        },
+                        exact_patterns = {
+                            -- Example for a specific filetype with Lua patterns
+                            -- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
+                            -- exactly match "impl_item" only)
+                            -- rust = true,
+                        },
+
+                        -- [!] The options below are exposed but shouldn't require your attention,
+                        --     you can safely ignore them.
+
+                        zindex = 20, -- The Z-index of the context window
+                        mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+                        -- Separator between context and content. Should be a single character string, like '-'.
+                        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+                        separator = nil
+                    })
+                end
+            },
             {'nvim-treesitter/nvim-treesitter-textobjects', build = ":TSUpdate"},
             {'nvim-treesitter/playground', build = ":TSUpdate"}
         },
@@ -50,7 +102,10 @@ return {
                             ["ac"] = "@class.outer",
                             -- You can optionally set descriptions to the mappings (used in the desc parameter of
                             -- nvim_buf_set_keymap) which plugins like which-key display
-                            ["ic"] = {query = "@class.inner", desc = "Select inner part of a class region"},
+                            ["ic"] = {
+                                query = "@class.inner",
+                                desc = "Select inner part of a class region"
+                            },
                             ["ig"] = "@conditional.inner",
                             ["ag"] = "@conditional.outer",
                             ["il"] = "@loop.inner",
@@ -85,7 +140,8 @@ return {
         end,
         keys = {
             {"<leader>th", "<cmd>TSBufToggle highlight<CR>", desc = "toggle treesitter highlights"},
-            {"<leader>tp", "<cmd>TSPlaygroundToggle<CR>", desc = "toggle treesitter playground"},
-        }
-    },
+            {"<leader>tp", "<cmd>TSPlaygroundToggle<CR>", desc = "toggle treesitter playground"}
+        },
+        event = {"BufReadPost"}
+    }
 }
