c8d0c35fd9c9bee6f359ff7212f56f99c19ff3c9
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-02-20 12:41:42 +0530

    undotree - add excluded_fts

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -5,7 +5,18 @@ return {
         {
             "<leader>u",
             function()
-                if vim.o.modifiable or vim.bo.filetype == "undotree" then
+                local excluded_fts = {
+                    "netrw",
+                    "harpoon",
+                    "fugitive",
+                    -- TODO add more here
+                }
+                for _, ft in ipairs(excluded_fts) do
+                    if vim.bo.filetype == ft then
+                        return ""
+                    end
+                end
+                if vim.o.modifiable or vim.bo.filetype == "undotree" or vim.bo.filetype == "diff" then
                     return "<cmd>UndotreeToggle<CR>"
                 else
                     return ""
