e4674b1b7b6af32fa3b8feb9719373c5613c2c22
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-02-23 08:46:59 +0530

    textobj - disable lazy load for now

--- nvim/lua/plugins/textobj.lua
+++ nvim/lua/plugins/textobj.lua
@@ -1,6 +1,7 @@
 return {
     'chrisgrieser/nvim-various-textobjs',
-    lazy = true,
+    -- TODO fix lazy load issue
+    lazy = false,
     opts = {
         lookForwardLines = 5, -- default: 5. Set to 0 to only look in the current line
         useDefaultKeymaps = false -- Use suggested keymaps (see README). Default: false.
