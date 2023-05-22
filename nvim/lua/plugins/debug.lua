97f6328e09bc94b3ca270e4a9d71241ee0aef5dd
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-05-22 11:39:51 +0530

    debug.lua - add go

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -1,10 +1,8 @@
 return {
     {
         'mfussenegger/nvim-dap',
-        dependencies = {
-            'rcarriga/nvim-dap-ui',
-        },
-        config = function ()
+        dependencies = {'rcarriga/nvim-dap-ui', 'leoluz/nvim-dap-go'},
+        config = function()
             local dap_ok, dap = pcall(require, 'dap')
             if not dap_ok then return end
 
@@ -34,11 +32,57 @@ return {
                     type = 'lldb',
                     request = 'launch',
                     program = function()
-                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug', 'file')
+                        return vim.fn.input('Path to executable: ',
+                            vim.fn.getcwd() .. '/target/debug', 'file')
                     end,
                     cwd = '${workspaceFolder}',
                     stopOnEntry = false,
                     args = {}
+                }
+            }
+            dap.adapters.go = function(callback, config)
+                local stdout = vim.loop.new_pipe(false)
+                local handle
+                local pid_or_err
+                local port = 38697
+                local opts = {
+                    stdio = {nil, stdout},
+                    args = {"dap", "-l", "127.0.0.1:" .. port},
+                    detached = true
+                }
+                handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+                    stdout:close()
+                    handle:close()
+                    if code ~= 0 then print('dlv exited with code', code) end
+                end)
+                assert(handle, 'Error running dlv: ' .. tostring(pid_or_err))
+                stdout:read_start(function(err, chunk)
+                    assert(not err, err)
+                    if chunk then
+                        vim.schedule(function()
+                            require('dap.repl').append(chunk)
+                        end)
+                    end
+                end)
+                -- Wait for delve to start
+                vim.defer_fn(function()
+                    callback({type = "server", host = "127.0.0.1", port = port})
+                end, 100)
+            end
+            dap.configurations.go = {
+                {type = "go", name = "Debug", request = "launch", program = "${file}"}, {
+                    type = "go",
+                    name = "Debug test", -- configuration for debugging test files
+                    request = "launch",
+                    mode = "test",
+                    program = "${file}"
+                }, -- works with go.mod packages and sub packages
+                {
+                    type = "go",
+                    name = "Debug test (go.mod)",
+                    request = "launch",
+                    mode = "test",
+                    program = "./${relativeFileDirname}"
                 }
             }
 
@@ -95,18 +139,23 @@ return {
             })
         end,
         keys = {
-            {"<leader>du", function() require('dapui').toggle() end, desc = "toggle dap ui"},
-            {"<leader>db", "<cmd>DapToggleBreakpoint<CR>", desc = "toggle debug breakpoint"},
             {
+                "<leader>du",
+                function()
+                    require('dapui').toggle()
+                end,
+                desc = "toggle dap ui"
+            }, {"<leader>db", "<cmd>DapToggleBreakpoint<CR>", desc = "toggle debug breakpoint"}, {
                 "<leader>dB",
-                function() require('dap').set_breakpoint(vim.fn.input('breakpoint condition:')) end,
+                function()
+                    require('dap').set_breakpoint(vim.fn.input('breakpoint condition:'))
+                end,
                 desc = "toggle conditional breakpoint"
-            },
-            {"<leader>dc", "<cmd>DapContinue<CR>", desc = "debug continue"},
+            }, {"<leader>dc", "<cmd>DapContinue<CR>", desc = "debug continue"},
             {"<leader>di", "<cmd>DapStepInto<CR>", desc = "debug step into"},
             {"<leader>do", "<cmd>DapStepOut<CR>", desc = "debug step out"},
             {"<leader>dO", "<cmd>DapStepOver<CR>", desc = "debug step over"},
-            {"<leader>dt", "<cmd>DapTerminate<CR>", desc = "terminate current debug session"},
+            {"<leader>dt", "<cmd>DapTerminate<CR>", desc = "terminate current debug session"}
         }
-    },
+    }
 }
