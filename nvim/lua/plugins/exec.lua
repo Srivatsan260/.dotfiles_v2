300ff56abe74abff17e9f0ed2271f2e7a1895414
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-05-29 07:43:14 +0530

    disable vim-jukit
    figure out how to set custom keymaps to re-enable

--- nvim/lua/plugins/exec.lua
+++ nvim/lua/plugins/exec.lua
@@ -35,16 +35,22 @@ return {
         'skywind3000/asyncrun.vim',
         cmd = "AsyncRun",
     },
-    {
-        'luk400/vim-jukit',
-        ft = {"json", "python"},
-        keys = {
-            -- TODO add more keymaps here
-            {"<leader>np", "<cmd>call jukit#convert#notebook_convert('jupyter-notebook')<cr>"},
-            -- {"<leader>ht", "<cmd>call jukit#convert#save_nb_to_file(0,1,'html')<cr>"},
-            -- {"<leader>rht", "<cmd>call jukit#convert#save_nb_to_file(1,1,'html')<cr>"},
-            -- {"<leader>pd", "<cmd>call jukit#convert#save_nb_to_file(0,1,'pdf')<cr>"},
-            -- {"<leader>rpd", "<cmd>call jukit#convert#save_nb_to_file(1,1,'pdf')<cr>"},
-        }
-    }
+    -- {
+    --     'luk400/vim-jukit',
+    --     ft = {"json", "python"},
+    --     init = function ()
+    --         vim.g.jukit_ipython = 0
+    --     end,
+    --     config = function ()
+    --         -- vim.g.jukit_terminal = "tmux"
+    --     end,
+    --     keys = {
+    --         -- TODO add more keymaps here
+    --         {"<leader>np", "<cmd>call jukit#convert#notebook_convert('jupyter-notebook')<cr>"},
+    --         -- {"<leader>ht", "<cmd>call jukit#convert#save_nb_to_file(0,1,'html')<cr>"},
+    --         -- {"<leader>rht", "<cmd>call jukit#convert#save_nb_to_file(1,1,'html')<cr>"},
+    --         -- {"<leader>pd", "<cmd>call jukit#convert#save_nb_to_file(0,1,'pdf')<cr>"},
+    --         -- {"<leader>rpd", "<cmd>call jukit#convert#save_nb_to_file(1,1,'pdf')<cr>"},
+    --     }
+    -- }
 }
