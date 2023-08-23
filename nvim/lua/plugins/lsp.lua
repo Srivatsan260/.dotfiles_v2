c27ee1e6da542ded750eaf734ccdf60072ad0dd9
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-08-23 23:19:09 +0530

    remove null-ls.nvim
    https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1621

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,30 +1,5 @@
 return {
     {
-        'jose-elias-alvarez/null-ls.nvim',
-        event = {"BufReadPost", "BufNewFile"},
-        config = function ()
-            local null_ls = require("null-ls")
-            local sources = {
-                -- code actions
-                null_ls.builtins.code_actions.gitsigns,
-                null_ls.builtins.code_actions.shellcheck,
-                -- formatters
-                null_ls.builtins.formatting.black,
-                null_ls.builtins.formatting.isort,
-                null_ls.builtins.formatting.lua_format,
-                null_ls.builtins.formatting.sqlfluff.with({extra_args = {"--dialect", "snowflake"}}),
-                -- diagnostics
-                null_ls.builtins.diagnostics.mypy, null_ls.builtins.diagnostics.flake8,
-                null_ls.builtins.diagnostics.rstcheck,
-                null_ls.builtins.diagnostics.sqlfluff.with({extra_args = {"--dialect", "snowflake"}}),
-                -- hovers
-                -- null_ls.builtins.hover.printenv
-            }
-            null_ls.setup({sources = sources})
-        end
-    },
-
-    {
         'VonHeikemen/lsp-zero.nvim',
         event = {"BufReadPre", "BufNewFile"},
         dependencies = {
