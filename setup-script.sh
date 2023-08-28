6dfc1d126339098c69a8aa5d43ee48db818f842c
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-08-28 22:42:10 +0530

    update terminal in setup_script.sh

--- setup-script.sh
+++ setup-script.sh
@@ -4,7 +4,7 @@ set -e
 
 [ ! -L ~/.vimrc ] && [ ! -e ~/.vimrc ] && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
 [ ! -L ~/.config/nvim ] && [ ! -e ~/.config/nvim ] && ln -s ~/dotfiles/nvim ~/.config/nvim
-[ ! -L ~/.config/kitty ] && [ ! -e ~/.config/kitty ] && ln -s ~/dotfiles/kitty ~/.config/kitty
+[ ! -L ~/.config/wezterm ] && [ ! -e ~/.config/wezterm ] && ln -s ~/dotfiles/wezterm ~/.config/wezterm
 [ ! -L ~/.config/lazygit ] && [ ! -e ~/.config/lazygit ] && ln -s ~/dotfiles/lazygit ~/.config/lazygit
 [ ! -L ~/.tmux.conf ] && [ ! -e ~/.tmux.conf ] && ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 [ ! -L ~/.gitignore_global ] && [ ! -e ~/.gitignore_global ] && ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
