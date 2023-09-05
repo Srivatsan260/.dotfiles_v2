02940d4eaa3b9179a1c8ddb9ca5be5ea490f2e94
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-09-05 09:42:36 +0530

    wezterm - update wallpaper_path

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,22 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 
 local function get_background()
     local home = os.getenv("HOME")
-    local wallpaper_path = string.gsub("~/Documents/wallpapers/anime/mythra.png", "~", "")
+    local wallpaper_path = string.gsub("~/dotfiles/wallpapers/butterfly.jpg", "~", "")
     local opts = {
         {
-            source = {File = home .. wallpaper_path},
+            source = { File = home .. wallpaper_path },
             repeat_x = "NoRepeat",
-            hsb = {brightness = 0.03},
-            vertical_align = "Middle"
-        }
+            hsb = { brightness = 0.08 },
+            vertical_align = "Middle",
+        },
     }
     return {}
     -- return opts
 end
 
 return {
-    audible_bell = 'Disabled',
+    audible_bell = "Disabled",
     max_fps = 120,
 
     hide_tab_bar_if_only_one_tab = true,
@@ -27,7 +27,7 @@ return {
     text_background_opacity = 0.90,
     window_decorations = "RESIZE",
 
-    color_scheme = 'Tomorrow Night Bright',
-    font = wezterm.font {family = 'JetbrainsMono Nerd Font Mono', weight = 'Medium'},
-    font_size = 15
+    color_scheme = "Tomorrow Night Bright",
+    font = wezterm.font({ family = "JetbrainsMono Nerd Font Mono", weight = "Medium" }),
+    font_size = 15,
 }
