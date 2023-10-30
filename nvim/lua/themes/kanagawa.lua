c02c480c984dbd547522011b7c5964b978818c5b
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2023-10-29 20:48:26 -0400

    fix theme file

--- nvim/lua/themes/kanagawa.lua
+++ nvim/lua/themes/kanagawa.lua
@@ -15,7 +15,6 @@ local theme_opts = {
     globalStatus = false, -- adjust window separators highlight for laststatus=3
     terminalColors = true, -- define vim.g.terminal_color_{0,17}
     colors = {},
-    overrides = {},
     theme = "default", -- Load "default" theme or the experimental "light" theme
 }
 
