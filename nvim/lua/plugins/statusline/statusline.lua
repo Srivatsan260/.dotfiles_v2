d4119b8cf616f75a0623fb0a6cc9f51ad2364bf2
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2024-03-28 20:15:41 -0400

    lualine - show @recording

--- nvim/lua/plugins/statusline/statusline.lua
+++ nvim/lua/plugins/statusline/statusline.lua
@@ -38,6 +38,11 @@ return {
                         "diagnostics",
                     },
                     lualine_x = {
+                        {
+                            require("noice").api.statusline.mode.get,
+                            cond = require("noice").api.statusline.mode.has,
+                            color = { fg = "#ff9e64" },
+                        },
                         "encoding",
                         "fileformat",
                         "filetype",
