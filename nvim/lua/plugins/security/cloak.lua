5aa6a0efe50a76c4e559730a6f67375108548f34
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2024-04-05 12:56:58 -0400

    cloak dbt profiles

--- nvim/lua/plugins/security/cloak.lua
+++ nvim/lua/plugins/security/cloak.lua
@@ -26,6 +26,20 @@ return {
                     -- If left empty the legacy behavior of keeping the first character is retained.
                     replace = nil,
                 },
+                {
+                    -- Match any file starting with '.env'.
+                    -- This can be a table to match multiple file patterns.
+                    file_pattern = { "profiles.yml" },
+                    -- Match an equals sign and any character after it.
+                    -- This can also be a table of patterns to cloak,
+                    -- example: cloak_pattern = { ':.+', '-.+' } for yaml files.
+                    cloak_pattern = ":.+",
+                    -- A function, table or string to generate the replacement.
+                    -- The actual replacement will contain the 'cloak_character'
+                    -- where it doesn't cover the original text.
+                    -- If left empty the legacy behavior of keeping the first character is retained.
+                    replace = nil,
+                }
             },
         }
         require("cloak").setup(opts)
