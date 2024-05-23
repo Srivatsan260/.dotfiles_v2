d8dcfbe27a1596f3d23b09a03377c0d1481d4be9
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2024-05-22 21:24:55 -0400

    update dbee

--- nvim/lua/plugins/database/dbee.lua
+++ nvim/lua/plugins/database/dbee.lua
@@ -3,7 +3,7 @@ return {
     dependencies = {
         "MunifTanjim/nui.nvim",
     },
-    keys = { {"<leader>DB", function() require("dbee").toggle() end, desc = "Open the DADBOD UI"} },
+    keys = { {"<leader>DB", function() require("dbee").toggle() end, desc = "Open the DBEE UI"} },
     build = function()
         -- Install tries to automatically detect the install method.
         -- if it fails, try calling it with one of these parameters:
