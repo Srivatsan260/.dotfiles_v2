735b1b843e08706e41e37bb69edc410dbe862a19
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2024-08-06 18:45:21 -0400

    markdown-preview

---
+++ nvim/lua/plugins/editor/markdown-preview.lua
@@ -0,0 +1,13 @@
+return {
+    "iamcco/markdown-preview.nvim",
+    lazy = true,
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+    keys = {
+        { "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", desc = "markdown preview" },
+    },
+}
