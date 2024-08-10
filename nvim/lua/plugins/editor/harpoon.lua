714245439d35ebf4cbbbbc128bc94c6d14e6bf3e
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2024-08-10 18:25:12 -0400

    fix harpoon

--- nvim/lua/plugins/editor/harpoon.lua
+++ nvim/lua/plugins/editor/harpoon.lua
@@ -1,7 +1,11 @@
 return {
     "ThePrimeagen/harpoon",
-    branch = "harpoon2",
+    commit = "e76cb03",
     opts = {
+        settings = {
+            sync_on_ui_close = true,
+            save_on_toggle = true,
+        },
         menu = {
             width = math.floor(vim.api.nvim_win_get_width(0) / 2)
         }
