e512056d7cec1fdf8c02892a71bbb18defeadb71
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2025-05-07 00:17:32 -0400

    update jq plugins

--- nvim/lua/plugins/editor/jq.lua
+++ nvim/lua/plugins/editor/jq.lua
@@ -1,7 +1,21 @@
 return {
-    -- TODO: Because the repo is unmaintained, vendor in the code from this repo and:
-        -- add JqClose
-        -- ???
-    "jrop/jq.nvim",
-    cmd = { "Jq" },
+    "yochem/jq-playground.nvim",
+    cmd = { "JqPlayground" },
+    output_window = {
+        split_direction = "right",
+        width = nil,
+        height = nil,
+        scratch = true,
+        filetype = "json",
+        name = "jq output",
+    },
+    query_window = {
+        split_direction = "below",
+        width = nil,
+        height = 0.3,
+        scratch = false,
+        filetype = "jq",
+        name = "jq query editor",
+    },
+    disable_default_keymap = false,
 }
