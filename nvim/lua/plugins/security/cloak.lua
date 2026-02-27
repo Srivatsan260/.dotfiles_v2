c731c83ef56fa53c013d2da8e1cea2aa21e9af8e
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2026-02-26 23:10:34 -0700

    update cloak.lua

--- nvim/lua/plugins/security/cloak.lua
+++ nvim/lua/plugins/security/cloak.lua
@@ -16,7 +16,7 @@ return {
                 {
                     -- Match any file starting with '.env'.
                     -- This can be a table to match multiple file patterns.
-                    file_pattern = { ".env*", ".config", "credentials", "*cfg", ".harlequin.toml" },
+                    file_pattern = { ".env*", ".config", "credentials", "*cfg", ".harlequin.toml", ".connections.env.sh" },
                     -- Match an equals sign and any character after it.
                     -- This can also be a table of patterns to cloak,
                     -- example: cloak_pattern = { ':.+', '-.+' } for yaml files.
