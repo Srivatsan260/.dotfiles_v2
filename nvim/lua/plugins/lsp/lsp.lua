b2fbe69eef66477620df5c3699ff9472da6eb15f
Srivatsan Ramaswamy <srivatsan260@gmail.com>
2026-05-22 10:23:03 -0700

    update plugins

--- nvim/lua/plugins/lsp/lsp.lua
+++ nvim/lua/plugins/lsp/lsp.lua
@@ -22,7 +22,12 @@ return {
             }
         },
         config = function()
-            require("mason").setup()
+            require("mason").setup({
+                registries = {
+                    "github:mason-org/mason-registry",
+                    "github:Crashdummyy/mason-registry",
+                },
+            })
             require("mason-lspconfig").setup({
                 handlers = {
                     -- this first function is the "default handler"
